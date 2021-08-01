00b4e74109cd3a6f5e16ca62f41c750278f61ad8
GeraJuarez <geracjuarez@gmail.com>
2021-07-31 17:15:31 -0700

    add graceful shutdown

--- key-store-api/main.go
+++ key-store-api/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"example/cloud-app/store/registry"
 	"example/cloud-app/store/router"
 	repository "example/cloud-app/store/usecase/repository/local"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -14,9 +17,28 @@ import (
 var PORT = "PORT"
 
 func main() {
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		signal := <-c
+		log.Printf("System call:%+v", signal)
+		cancel()
+	}()
+
+	if err := serve(ctx); err != nil {
+		log.Printf("Failed to serve:%+v\n", err)
+	}
+}
+
+func serve(ctx context.Context) error {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Fatalf("Error loading .env file %+v\n", err)
 	}
 
 	portEnv := os.Getenv(PORT)
@@ -25,5 +47,37 @@ func main() {
 	registry := registry.NewRegistry(datastore)
 	router := router.Start(registry.NewAppController())
 
-	log.Fatal(http.ListenAndServe(":"+portEnv, router))
+	srv := &http.Server{
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		Addr:         ":" + portEnv,
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      router,
+	}
+
+	go func() {
+		// Run our server in a goroutine so that it doesn't block.
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	log.Printf("server started")
+	<-ctx.Done()
+	log.Printf("server stopped")
+
+	// Create a deadline to wait for current requests to complete
+	ctxShutdown, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	if err := srv.Shutdown(ctxShutdown); err != nil {
+		log.Fatalf("server Shutdown Failed:%+s", err)
+	}
+	log.Printf("server exited properly")
+
+	if err == http.ErrServerClosed {
+		err = nil
+	}
+	return err
 }
